901d9ae3f77c7df2fc49bc2e71119048235a22e5
saku-bruh <notwolfyy@gmail.com>
2023-02-13 17:14:34 +0200

    extra: add experimental features
    Adding & enabling the Experimental "SonyHiRes" feature which brings Audio Settings from the 1 IV

--- extra.mk
+++ extra.mk
@@ -11,6 +11,21 @@ PRODUCT_SOONG_NAMESPACES += \
     Camera \
     CameraCommon \
     CameraExtensions
+   
+# SonyHiRes
+PRODUCT_COPY_FILES += \
+   $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/sonyhires/system/,$(TARGET_COPY_OUT_SYSTEM)/) \
+   $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/sonyhires/system_ext/,$(TARGET_COPY_OUT_SYSTEM_EXT)/) \
+   $(call find-copy-subdir-files,*,$(LOCAL_PATH)/extra/sonyhires/vendor/,$(TARGET_COPY_OUT_VENDOR)/)
+
+# SonyHiRes PKGs
+ PRODUCT_PACKAGES += \
+    SoundEnhancement \
+    360RASystem \
+    360RASettings \
+    AudioUtil \
+    daxUI \
+    daxService
 
 # Controller Support
  PRODUCT_COPY_FILES += \
